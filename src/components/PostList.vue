c0a9d83db0ba4a23ac4c8b08065a4ed3f4bf449a
Koji Eguchi <koji.eguchi@hotmail.com>
2023-05-22 14:52:41 -0400

    first commit

---
+++ src/components/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>HelloWorld</h1>
+  <div class="flex">
+    <PostListItems v-for="post in posts" :key="post.id" :post="post" class="card w-75 mx-auto my-3"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import PostListItems from './PostListItems.vue';
+export default {
+  // data(): This is a method in the Vue component that is responsible for defining and returning the initial data for the component.
+  data() {
+    // return { posts: [] };: Within the data() method, an object is returned, which serves as the initial state for the component. In this case, the object contains a single property called posts, which is an empty array [].
+    return {
+      posts: []
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      this.posts = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  props: [],
+  components: {PostListItems},
+  name: 'PostList'
+};
+
+</script>
+
+<style></style>
+
---
+++ src/components/PostListItems.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>{{ post.title }}</h3>
+    <p>{{ post.body }}</p>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+ 
+  props: ["post"],
+  components: {},
+  name: 'PostListItems'
+};
+
+</script>
+
+<style></style>
+
